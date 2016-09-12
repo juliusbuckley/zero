9e8c1a41835580b9080f23b3e201905ed2ade274
juliusbuckley <juliusbuckley@gmail.com>
2016-09-11 20:29:11 -0700

    chores/ Made code more modular.

--- server/routes/slackRoutes.js
+++ server/routes/slackRoutes.js
@@ -1,37 +1,9 @@
-import axios from 'axios';
-import { addUrl, parseData } from '../db/utils';
+import { getMessages } from '../controllers/messagesController';
 
 //API ROUTES
 const slackRoutes = (app) => {
-  //////////////////////////////////////////////
   //SLACK MESSAGES ROUTE
-  //////////////////////////////////////////////
-  app.get('/messages', (req,res) => {
-    //GET request to slack API endpoint for all messages containing search query
-    axios.get(`https://slack.com/api/search.messages?token=${process.env.SLACK_TOKEN}&query=http&pretty=1`)
-      .then((response) => {
-        response.data.messages.matches.forEach((message) => {
-          for (let key in message) {
-            if (key === 'attachments') {
-              addUrl('urls', message[key][0].title, message[key][0].title_link);
-            } else if (key === 'previous') {
-                parseData(message[key]);
-            } else if (key === 'previous_2') {
-                parseData(message[key]);
-            } else if (key === 'next') {
-                parseData(message[key]);
-            } else if (key === 'next_2') {
-                parseData(message[key]);
-            }
-          }
-        });
-        res.send(JSON.stringify('test'));
-      })
-      .catch((err) => {
-        console.log(`Error: ${err}`);
-      });
-  });
-
+  app.get('/messages', getMessages);
 };
 
 export default slackRoutes;
