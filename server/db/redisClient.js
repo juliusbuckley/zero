fd6b3cfe34c10bb8010b2d534a9239bd795a1814
juliusbuckley <juliusbuckley@gmail.com>
2016-10-11 11:18:39 -0700

    chores/ Added deployed redis instance.

--- server/db/redisClient.js
+++ server/db/redisClient.js
@@ -1,5 +1,15 @@
 import redis from 'redis';
 
-const client = redis.createClient();
+const client = redis.createClient(process.env.REDIS_PORT, process.env.REDIS_HOSTNAME, {no_ready_check: true});
+
+client.auth(process.env.REDIS_PASSWORD, (err) => {
+  if (err) {
+    console.log(`error: ${err}`);
+  }
+});
+
+client.on('connect', () => {
+  console.log(`connected to Redis on port: ${process.env.REDIS_PORT}`);
+});
 
 export default client;
