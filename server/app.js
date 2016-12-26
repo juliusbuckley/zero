202a5c0d2f0f506513f908c2484eda9033a9b0e0
juliusbuckley <juliusbuckley@gmail.com>
2016-12-26 15:24:08 -0500

    chores/ connected redis db

--- server/app.js
+++ server/app.js
@@ -1,14 +1,13 @@
 import express from 'express';
 import path from 'path';
 import slackRoutes from './routes/slackRoutes';
-// import redis from './db/redisClient';
-// redis.flushall();
+import redis from './db/redisClient';
 
+redis.flushall();
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 app.use(express.static(path.join(__dirname, '../client')));
-
 slackRoutes(app);
 
 app.listen(port, () => {
