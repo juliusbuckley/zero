4eb9153fd42f31391e41c9f94cdeaa1933792aa0
juliusbuckley <juliusbuckley@gmail.com>
2016-12-26 15:14:35 -0500

    chores/ updated title on index

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import path from 'path';
 import slackRoutes from './routes/slackRoutes';
-import redis from './db/redisClient';
+// import redis from './db/redisClient';
 
 redis.flushall();
 
